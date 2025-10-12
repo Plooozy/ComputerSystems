e56e4fee7f1249c2422cf6cbb761e8c832b4c83a
Nick <lolipop22234@gmail.com>
2025-10-12 13:58:56 +0300

    Manual-commit: changes

---
+++ week_3/commitPush.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Checking for changes
+
+
+if [[]]; then
+    echo "There are changes"
+else
+    echo "There are no changes"
---
+++ week_3/test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Hello, world!"
+
+x="my name is Nick"
+
+echo '$x'
+echo "$x"
+
+echo "${x}"
+echo "${x:0:5}"
+echo "${#x}"
