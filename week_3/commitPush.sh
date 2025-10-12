534683323638641d32d8892a5a081680788e8892
Nick <lolipop22234@gmail.com>
2025-10-12 14:20:51 +0300

    Manual-commit: changes

--- week_3/commitPush.sh
+++ week_3/commitPush.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+# Getting status
+output=$(git status --porcelain)
 # Checking for changes
-
-
-if [[]]; then
-    echo "There are changes"
+if [ "$output" = "" ]; then
+    echo "No changes"
 else
-    echo "There are no changes"
+    echo "There are changes"
+    git add .
+    git commit -m "Auto-commit on $(date)"
+    output=$(git status --porcelain)
+    if [ "$output" = "" ]; then
+        echo "Succesfull commit"
+    else
+        echo "Error: commit failed"
+    fi
+    git push origin master
